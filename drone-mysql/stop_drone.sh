33916bd68bf3f29ebe9c38bdfa7a5c4b8e3da2ce
yangwenmai <yangwen.yw@gmail.com>
2018-08-24 16:31:22 +0800

    运行脚本

--- drone-mysql/stop_drone.sh
+++ drone-mysql/stop_drone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "docker stop ..."
-docker stop drone-mysql_mysql-server_1 drone-mysql_drone-server_1 drone-mysql_drone-agent_1
+docker stop drone-mysql_mysql-server_1 drone-mysql_drone-server_1 drone-mysql_drone-agent-1_1 drone-mysql_drone-agent-2_1
 echo "docker rm ..."
-docker rm drone-mysql_mysql-server_1 drone-mysql_drone-server_1 drone-mysql_drone-agent_1
+docker rm drone-mysql_mysql-server_1 drone-mysql_drone-server_1 drone-mysql_drone-agent-1_1 drone-mysql_drone-agent-2_1
 echo "stop docker completed."
---
+++ mongodb/stop_mongodb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop mongodb
+docker rm mongodb

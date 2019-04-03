6f57d578a99074bcd073fb1ed26748541e40d180
yangwenmai <yangwen.yw@gmail.com>
2019-04-03 09:18:18 +0800

    feat(README): update README by go run main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	baseStr := "# docker/docker-compose/kubernetes scripts\n"
+
+	r := ""
+	for _, f := range files {
+		if f.IsDir() {
+			r += fmt.Sprintf("- [%s](./%s)\n", f.Name(), f.Name())
+		}
+	}
+	ret := baseStr + "\n" + r
+	ioutil.WriteFile("README.md", []byte(ret), 0666)
+}
